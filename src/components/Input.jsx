e413a9b93957b9b92ec067044dd99a1613c81810
Illyrian1000 <52840976+Illyrian1000@users.noreply.github.com>
2023-12-05 20:00:16 +0100

    fixed interest not showing as number

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -36,7 +36,7 @@ export default function Input(props) {
 
   function submitHandler(e) {
     e.preventDefault();
-    if (amount !== 0 && period !== 0 && interest !== 0) {
+    if (amount !== 0 && period !== 0 && Number(interest) !== 0) {
       let dataObj = {
         passAmount: amount,
         passPeriod: period,
